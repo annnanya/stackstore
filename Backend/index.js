1bed25405e6cdcb2303ab391e9da4194c853fa75
annnanya <singhananya1616@gmail.com>
2024-11-24 20:36:42 +0530

    ref#123 - added login and signup apis

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bookRoute from "./route/book.route.js";
-// import userRoute from "./route/user.route.js";
+import userRoute from "./route/user.route.js";
 import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 const port = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI;
 
@@ -26,7 +27,7 @@ connectDB();
 
 // defining routes
 app.use("/book", bookRoute);
-// app.use("/user", userRoute);
+app.use("/user", userRoute);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
